90db799686d07855b2d22fee147bea8ac549bcec
Scavengerizer <himanshuthesuperb@gmail.com>
2020-11-17 21:25:39 +0530

    cleaned the old files, no warnings/errors

--- src/components/01 header.js
+++ src/components/01 header.js
@@ -1,25 +1,9 @@
 import React from "react";
-import { Link } from "react-router-dom";
 
 function Header() {
   return (
     <header>
-      <div className='header_contents col center'>
-        <div className='logo'>
-          <span>Photography</span>
-        </div>
-        <div className='nav-links row'>
-          <Link to='/' className='link'>
-            Home
-          </Link>
-          <Link to='/work' className='link'>
-            Work
-          </Link>
-          <Link to='/contact' className='link'>
-            Contact Us
-          </Link>
-        </div>
-      </div>
+      <div className='hamburger-menu'></div>
     </header>
   );
 }

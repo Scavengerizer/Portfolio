cf7a61b9c8f0905d073c38bd0c9cd16960351477
Scavengerizer <himanshuthesuperb@gmail.com>
2020-11-17 22:37:09 +0530

    custom cursor working

--- src/components/01 header.js
+++ src/components/01 header.js
@@ -1,10 +1,24 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 function Header() {
+  useEffect(() => {
+    document.addEventListener("mousemove", (e) => {
+      const cursor = document.querySelector(".cursor");
+      const cursorFollower = document.querySelector(".cursor-follower");
+      cursor.style.left = `${e.clientX}px`;
+      cursor.style.top = `${e.clientY}px`;
+      cursorFollower.style.left = `${e.clientX}px`;
+      cursorFollower.style.top = `${e.clientY}px`;
+    });
+  }, []);
   return (
-    <header>
-      <div className='hamburger-menu'></div>
-    </header>
+    <div className='header'>
+      <div className='cursor'></div>
+      <div className='cursor-follower'></div>
+      <header>
+        <div className='hamburger-menu'></div>
+      </header>
+    </div>
   );
 }
 

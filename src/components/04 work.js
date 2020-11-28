0825ffc465084c4650f9e22c754a44d9a061985e
Scavengerizer <himanshuthesuperb@gmail.com>
2020-11-28 15:01:09 +0530

    added parallax and completed projects section

--- src/components/04 work.js
+++ src/components/04 work.js
@@ -1,6 +1,50 @@
-import React from "react";
+import React, { useEffect } from "react";
+import Rellax from "rellax";
 
 function Work() {
+  useEffect(() => {
+    new Rellax(".img1", {
+      speed: -5,
+      center: true,
+      wrapper: null,
+      round: true,
+      vertical: true,
+      horizontal: false,
+    });
+    new Rellax(".img2", {
+      speed: -5,
+      center: true,
+      wrapper: null,
+      round: true,
+      vertical: true,
+      horizontal: false,
+    });
+    new Rellax(".img3", {
+      speed: -5,
+      center: true,
+      wrapper: null,
+      round: true,
+      vertical: true,
+      horizontal: false,
+    });
+    new Rellax(".img4", {
+      speed: -5,
+      center: true,
+      wrapper: null,
+      round: true,
+      vertical: true,
+      horizontal: false,
+    });
+    new Rellax(".case-text", {
+      speed: 4,
+      center: true,
+      wrapper: null,
+      round: true,
+      vertical: true,
+      horizontal: false,
+    });
+  }, []);
+
   return (
     <div className='work'>
       <div className='container'>
@@ -8,7 +52,35 @@ function Work() {
           <div className='work-heading'>
             <h1>My Work</h1>
           </div>
-          <div className='cases row center'>
+          <div className='cases row'>
+            <div className='case-section2'>
+              <div className='case'>
+                <div className='col'>
+                  <h3>Home Decor</h3>
+                  <div className='img-prop'>
+                    <div className='img1'></div>
+                  </div>
+                </div>
+              </div>
+
+              <div className='case'>
+                <div className='col'>
+                  <h3>Photopics</h3>
+                  <div className='img-prop'>
+                    <div className='img3'></div>
+                  </div>
+                </div>
+              </div>
+
+              <div className='case-text'>
+                <h4>The Design</h4>
+                <p>
+                  Each website is uniquely<br></br> crafted with appealing
+                  colour<br></br> combination and sleek and<br></br> catchy
+                  design.
+                </p>
+              </div>
+            </div>
             <div className='case-section1'>
               <div className='case-text'>
                 <h4>The Design</h4>
@@ -21,14 +93,22 @@ function Work() {
 
               <div className='case'>
                 <div className='col'>
-                  <h3>Home Decor</h3>
-                  <div className='col center img-prop'>
-                    <div className='img1'></div>
+                  <h3>Gaming Website</h3>
+                  <div className='img-prop'>
+                    <div className='img2'></div>
+                  </div>
+                </div>
+              </div>
+
+              <div className='case'>
+                <div className='col'>
+                  <h3>KYC Interface</h3>
+                  <div className='img-prop'>
+                    <div className='img4'></div>
                   </div>
                 </div>
               </div>
             </div>
-            <div className='case-section2'></div>
           </div>
         </div>
       </div>

2b240c240bc41fc28d0a6ac559659b2c3efc7d8b
Scavengerizer <himanshuthesuperb@gmail.com>
2020-11-28 12:42:08 +0530

    working on project section

--- src/components/04 work.js
+++ src/components/04 work.js
@@ -3,7 +3,35 @@ import React from "react";
 function Work() {
   return (
     <div className='work'>
-      <div className='work-text'></div>
+      <div className='container'>
+        <div className='col'>
+          <div className='work-heading'>
+            <h1>My Work</h1>
+          </div>
+          <div className='cases row center'>
+            <div className='case-section1'>
+              <div className='case-text'>
+                <h4>The Design</h4>
+                <p>
+                  Each website is uniquely<br></br> crafted with appealing
+                  colour<br></br> combination and sleek and<br></br> catchy
+                  design.
+                </p>
+              </div>
+
+              <div className='case'>
+                <div className='col'>
+                  <h3>Home Decor</h3>
+                  <div className='col center img-prop'>
+                    <div className='img1'></div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className='case-section2'></div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
